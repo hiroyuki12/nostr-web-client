c611ddad7cf408af1688f268e13a85fc02f8d202
hiroyuki12 <hiroyuki12@gmail.com>
2023-07-23 00:39:40 +0900

    Update relay.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,30 +5,37 @@ import { NostrProvider } from "nostr-react";
 const relayUrls = [
 
 
-  "wss://relay-jp.nostr.wirednet.jp",   //jp 
-      "wss://yabu.me",                  //en, jp, (wirednet,heguro,h3z,holybea,ocha,fediverce)
-      "wss://nostr.wine",               //jp en, Vitor, Terry
+//  "wss://relay-jp.nostr.wirednet.jp",   //jp 
       "wss://relay.mostr.pub",          // nachika
-      "wss://nos.lol",                  //en, jp  Nelly
+      "wss://yabu.me",                  //en, jp, (wirednet,heguro,h3z,holybea,ocha,fediverce)
+      "wss://nos.lol",                  //en, jp  jack, Will, Nelly
+//      "wss://relay.nostr.band",         //NIP-50 search, en, jp nachika
+//      "wss://nostr.wine",               //jp en, Vitor, Terry
+//      
 
-      //
-  /*"wss://relay.damus.io",           //en, jp  Nelly
+
+//     "nostrja-kari-nip50.heguro.com",   // NIP-50
+//      "wss://nrelay-jp.c-stellar.net",
+//      "wss://nrelay.c-stellar.net",
+      
+/*  "wss://relay.damus.io",           //en, jp  Nelly
   "wss://eden.nostr.land",
   "wss://nostr.fmt.wiz.biz",
   "wss://nos.lol",                  //en, jp,
-  "wss://relay.snort.social",*/
-      /*"wss://relay.nostr.band",         //en, jp nachika
+  "wss://relay.snort.social",
       "wss://nostr.holybea.com",        //jp
       "wss://nostr.fediverse.jp",       //jp 
       "wss://nostr-relay.nokotaro.com", //jp, en 
       "wss://nostr.h3z.jp",             //en, jp 
-      "wss://nostr-paid.h3z.jp",*/        //jp,
-      
+      "wss://nostr-paid.h3z.jp",        //jp,
+*/      
+
+//"wss://relay.nostr.moctane.com",
 
 
 
-  /*"wss://nostrja-kari.heguro.com",  //jp
-  "wss://relay.nostrich.land",  //jp en
+  /*"wss://relay.nostrich.land",  //jp en
+  "wss://nostrja-kari.heguro.com",  //jp
   "wss://relay.nostrica.com",   //en
   "wss://welcome.nostr.wine",   //en jp
   "wss://nostr.h3y6e.com",
