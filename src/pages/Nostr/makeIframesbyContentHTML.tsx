51074ee186011d09868fe20a36c5362fd8563cc7
hiroyuki12 <hiroyuki12@gmail.com>
2025-02-20 22:04:11 +0900

    fix Twitter iframe by content.

--- src/pages/Nostr/makeIframesbyContentHTML.tsx
+++ src/pages/Nostr/makeIframesbyContentHTML.tsx
@@ -85,8 +85,16 @@ export const makeIframesbyContentHTML = (content, note) => {
 
       // Twitter iframe
       else if(splitContent[i].includes("twitter.com") || splitContent[i].includes("x.com")) {
-        const id = splitContent[i].replace("x.com","twitter.com"); 
-        const tmpIframe = '<iframe border=0 frameborder=0 height=387 width=563 src="https://twitframe.com/show?url=' + id + '"></iframe>'
+        // const id = splitContent[i].replace("x.com","twitter.com"); 
+        // const tmpIframe = '<iframe border=0 frameborder=0 height=387 width=563 src="https://twitframe.com/show?url=' + id + '"></iframe>'
+
+        const tmp = splitContent[i].split('?')[0]
+        const id = tmp.split('/')[5]  //1892199000356655161
+        const embedid = `https://platform.twitter.com/embed/Tweet.html?dnt=false&embedId=twitter-widget-3&features=eyJ0ZndfdGltZWxpbmVfbGlzdCI6eyJidWNrZXQiOlsibGlua3RyLmVlIiwidHIuZWUiLCJ0ZXJyYS5jb20uYnIiLCJ3d3cubGlua3RyLmVlIiwid3d3LnRyLmVlIiwid3d3LnRlcnJhLmNvbS5iciJdLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X2hvcml6b25fdGltZWxpbmVfMTIwMzQiOnsiYnVja2V0IjoidHJlYXRtZW50IiwidmVyc2lvbiI6bnVsbH0sInRmd190d2VldF9lZGl0X2JhY2tlbmQiOnsiYnVja2V0Ijoib24iLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X3JlZnNyY19zZXNzaW9uIjp7ImJ1Y2tldCI6Im9uIiwidmVyc2lvbiI6bnVsbH0sInRmd19jaGluX3BpbGxzXzE0NzQxIjp7ImJ1Y2tldCI6ImNvbG9yX2ljb25zIiwidmVyc2lvbiI6bnVsbH0sInRmd190d2VldF9yZXN1bHRfbWlncmF0aW9uXzEzOTc5Ijp7ImJ1Y2tldCI6InR3ZWV0X3Jlc3VsdCIsInZlcnNpb24iOm51bGx9LCJ0Zndfc2Vuc2l0aXZlX21lZGlhX2ludGVyc3RpdGlhbF8xMzk2MyI6eyJidWNrZXQiOiJpbnRlcnN0aXRpYWwiLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X2V4cGVyaW1lbnRzX2Nvb2tpZV9leHBpcmF0aW9uIjp7ImJ1Y2tldCI6MTIwOTYwMCwidmVyc2lvbiI6bnVsbH0sInRmd19kdXBsaWNhdGVfc2NyaWJlc190b19zZXR0aW5ncyI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9LCJ0ZndfdmlkZW9faGxzX2R5bmFtaWNfbWFuaWZlc3RzXzE1MDgyIjp7ImJ1Y2tldCI6InRydWVfYml0cmF0ZSIsInZlcnNpb24iOm51bGx9LCJ0Zndfc2hvd19ibHVlX3ZlcmlmaWVkX2JhZGdlIjp7ImJ1Y2tldCI6Im9mZiIsInZlcnNpb24iOm51bGx9LCJ0ZndfdHdlZXRfZWRpdF9mcm9udGVuZCI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9fQ%3D%3D&frame=false&hideCard=false&hideThread=false&id=${
+            id
+          }`;
+        const tmpIframe = '<iframe id="' + id + '" border=0 frameborder=0 height=387 width=563 src="' + embedid + '" />'
+
 
         content = content.replace(splitContent[i], tmpIframe);
 
@@ -104,9 +112,11 @@ export const makeIframesbyContentHTML = (content, note) => {
       else if (splitContent[i].includes('http')){ // (instagram etc)
       // else if(!splitContent[i].includes('nostrudel.ninja')  // #t
         // && (splitContent[i].includes('media.unnerv.jp') ||
-        //  splitContent[i].includes('nostr-hotter-site.vercel.app'))) {
+        //  )) {
         
-        if(!splitContent[i].includes('domain'))  // todo: https://<domain>/.well-known/lnurlp/<username>
+        if(!splitContent[i].includes('domain') && !splitContent[i].includes('www.amazon.co.jp')
+          && !splitContent[i].includes('nostr-hotter-site.vercel.app') )
+          // todo: https://<domain>/.well-known/lnurlp/<username>
         {
           httpLinkUrl1 = splitContent[i].substring(splitContent[i].indexOf('https://'), splitContent[i].length);
 
@@ -116,6 +126,15 @@ export const makeIframesbyContentHTML = (content, note) => {
           content = content.replace(httpLinkUrl1, '');
           // (c)_ttp' 表示
           content = content.replace(httpLinkUrl1, httpLinkUrl1.replace('http', '(c)_ttp'));
+        }
+        else {
+          httpLinkUrl1 = splitContent[i].substring(splitContent[i].indexOf('https://'), splitContent[i].length);
+
+          // (c)_ttp' 表示
+          const tmpIframe = '<a href="' + httpLinkUrl1 + '" target="_blank">' + httpLinkUrl1.replace('http', '(c)_ttp') + '</a>'
+          iframe1 = tmpIframe;
+          // URL 非表示
+          content = content.replace(httpLinkUrl1, '');
         }
       }
 
